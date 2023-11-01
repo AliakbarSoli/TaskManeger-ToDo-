f556c0df8743144205bde60daf55e34d2042533a
aliakbar <aliakbarsoltaniiiii@gmail.com>
2023-11-01 12:15:47 +0330

    Add Create_Info_txt_File && Create Object In Program.cs

---
+++ CRUD/Create_Info_txt(File).cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManeger_01.CRUD
+{
+    internal class Create_Info_txt_File_
+    {
+        public Create_Info_txt_File_() 
+        {
+            
+        }  
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using TaskManeger_01.CRUD;
 
 Read_Info read_Info = new Read_Info();
+Create_Info_txt_File_ create_Info_Txt_File=new Create_Info_txt_File_();
 Console.ReadKey();
