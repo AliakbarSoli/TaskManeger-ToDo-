916cdff5913b9510255fa74a26f49694f965ad7c
aliakbar <aliakbarsoltaniiiii@gmail.com>
2023-11-04 17:11:10 +0330

    Add && Fix The Read File Save (txt) File

--- CRUD/Read_Info.cs
+++ CRUD/Read_Info.cs
@@ -7,49 +7,66 @@ using System.Threading.Tasks;
 
 namespace TaskManeger_01.CRUD
 {
-    internal class Read_Info
+    internal class Save_Info
     {
-        char[] sta_End = new char[] {'a','p','A','P' };
-       public Read_Info()
-       {
+        internal Save_Info() 
+        {
+
+        }
+        
+    }
+    internal class Read_Info 
+    {
+        public Read_Info()
+        {
             Console.WriteLine("Task : ");
             string NameTask = Console.ReadLine();
-
+                            
+            
             Console.WriteLine("When Will It Start : ");
-            string Start_Time = Console.ReadLine();
+            double Start_Time =Convert.ToDouble( Console.ReadLine());           
+                      
+             Console.WriteLine("When Will It End : ");
+             double End_Time = Convert.ToDouble(Console.ReadLine());
+             
 
-            Console.WriteLine("AM Or PM (A,P) ");
-            char start = Convert.ToChar(Console.ReadLine());
-
-            for (int i = 0; i <= 3; i++)
+             StreamReader Save_Count = new StreamReader(@"D:\TaskMan\Counter.txt");
+             var Count = Save_Count.ReadLine();
+             Save_Count.Close();
+             StreamWriter Save_Information = new StreamWriter($@"D:\TaskMan\{Count}.txt");
+            string start_AP;
+            string end_AP;
+            if (Start_Time>=12)
             {
-                
-                    if (start == sta_End[i])
-                    {  
-                         Console.WriteLine("When Will It End : ");
-                         string End_Time = Console.ReadLine();
-
-                         Console.WriteLine("AM Or PM (A,P) ");
-                         char End = Convert.ToChar(Console.ReadLine());
-
-                         for (global::System.Int32 j = 0; j <= 3; j++)
-                         {
-                             if (End== sta_End[j])
-                             {
-                            Console.WriteLine("Done! Information Saved");
-                             }
-                         }
-                    break;
-                    }
-                    else
-                    {
-                    Console.WriteLine("Please Enter TheCorrect Character (a,A,p,P)");
-                    break;
-                    }
+                 
+                start_AP=Start_Time.ToString() + " " + "PM";
             }
+            else
+            {
+                start_AP = Start_Time.ToString() + " " + "AM";
+            }
+            if (End_Time>=12) 
+            {
+                end_AP = End_Time.ToString() +" "+ "PM";
+            }
+            else
+            {
+                end_AP = End_Time.ToString() + " " + "AM";
+            }
+            
+             string[] Property_Task_title = new string[] { "Task : ", "Start Time : " , "End Time : "};
+            string[] Property_Task = new string[] { NameTask, start_AP, end_AP };
 
+             Console.WriteLine("Done! Information Saved");
 
-                    Console.ReadKey();
-       }
+            for (global::System.Int32 k = 0; k < Property_Task.Length; k++)
+            {
+                 Save_Information.Write(Property_Task_title[k]);
+                 Save_Information.WriteLine(Property_Task[k]);
+            }
+             Save_Information.Close();
+                             
+            Console.ReadKey();
+        }
     }
 }
